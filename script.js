5cc2bb12044f5306abf2307a59a08ee42b95217c
sonny-lab <Willryan73@gmail.com>
2025-07-21 15:00:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// This script will contain Politicraft's core game logic.
+// It will be loaded by the game setup and dashboard pages (e.g., create_game.html, game_dashboard.html).
+// For the initial Home Page (index.html), logic is in home.js.
